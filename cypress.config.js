e8cd2f82f5823e95a335264513250b0cec8725ca
Chisel <chisel@malik-wright.uk>
2025-03-24 11:52:40 +0000

    chore: update cypress project id

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
+  projectId: '4s4hw6',
   e2e: {
     baseUrl: 'http://localhost:8006',
     supportFile: 'cypress/support/e2e.js',
